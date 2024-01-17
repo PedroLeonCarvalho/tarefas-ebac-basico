26e45146819a9bbbf42a349b7f12bbd3c688d42b
Pedro Leon <pedrolbbcarvalho@gmail.com>
2024-01-17 16:47:34 -0300

    Initial commit

---
+++ src/main/java/br/com/ebac/Usuario.java
@@ -0,0 +1,41 @@
+package br.com.ebac;
+
+public class Usuario {
+
+    private String nome;
+    private String cpf;
+    private int idade;
+    private Long telefone;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+}
