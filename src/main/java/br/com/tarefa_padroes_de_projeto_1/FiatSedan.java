260341935be9c5ade7a7b3486fad2b9e52f86fd4
Pedro Leon <pedrolbbcarvalho@gmail.com>
2024-05-01 15:13:30 -0300

    mod30

--- src/main/java/br/com/tarefa_padroes_de_projeto_1/FiatSedan.java
+++
@@ -1,8 +0,0 @@
-package br.com.tarefa_padroes_de_projeto_1;
-
-public class FiatSedan implements Sedan {
-    @Override
-    public void portaMalasGrande() {
-        System.out.println("FIatSedan");
-    }
-}
--- src/main/java/br/com/tarefa_padroes_de_projeto_1/ToyotaSedan.java
+++
@@ -1,8 +0,0 @@
-package br.com.tarefa_padroes_de_projeto_1;
-
-public class ToyotaSedan implements Sedan {
-    @Override
-    public void portaMalasGrande() {
-        System.out.println("Toyota Sedan");
-    }
-}
